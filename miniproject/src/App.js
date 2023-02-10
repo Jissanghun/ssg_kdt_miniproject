70ecbc9792e7d22ca0438431879c47664f41cc9b
Jissanghun <ingingking_@naver.com>
2023-02-10 15:16:11 +0900

    detail

--- miniproject/src/App.js
+++ miniproject/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Main from './components/pages/Main';
 import ProductList from './components/pages/ProductList';
+import Footer from '/componets/layout/Footer';
+import Header from '/componets/layout/Header';
+import Cart from '/componets/pages/Cart';
+
 
 function App() {
   return (
---
+++ miniproject/src/components/pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Cart() {
+    return (
+      <>Main page</>
+    )
+  }
+  
+  export default Main;
